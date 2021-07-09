7450710769870b295997515f52e194cac378a632
Deanna Loreti <dloreti@zivaro.com>
2021-07-09 14:34:02 -0600

    Updated to include withRouter functionality and passing in the linkUrl

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,35 +12,35 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     size: 'small',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                   },
                   {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     size: 'small',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: 'jackets'
                   },
                   {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     size: 'small',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: 'sneakers'
                   },
                   {
                     title: 'womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: 'womens'
                   },
                   {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: 'mens'
                   }
             ],
         }
@@ -49,8 +49,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
                     ))
                 }
             </div>
