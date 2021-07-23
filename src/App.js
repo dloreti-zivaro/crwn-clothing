ded7e01927f0ee3514c04b7b52d9271b738f3856
Deanna Loreti <dloreti@zivaro.com>
2021-07-23 14:09:58 -0600

    Add header component, crown logo, and made links black and no underline

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
-      <Switch>
-       <Route  exact path='/' component ={HomePage}/>
-       <Route  exact path='/shop' component ={ShopPage}/>
-      </Switch>
+      <Header />
+        <Switch>
+        <Route  exact path='/' component ={HomePage}/>
+        <Route  exact path='/shop' component ={ShopPage}/>
+        </Switch>
+
     </div>
   );
 }
