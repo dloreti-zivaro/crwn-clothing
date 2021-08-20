515ae5e98a3bd26d81e1f4a5a1f016f987339c12
Deanna Loreti <dloreti@zivaro.com>
2021-08-20 13:26:43 -0600

    Added the sign-up with email and password functionality

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -11,7 +11,35 @@ const config = {
     messagingSenderId: "1096461141566",
     appId: "1:1096461141566:web:9e305bcadd0c2d7384336f",
     measurementId: "G-GK06KHT13C"
-  };
+};
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+  if(!snapShot.exists) {
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName, 
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }
+    catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 
   firebase.initializeApp(config);
 
